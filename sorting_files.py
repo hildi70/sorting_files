98a16d330b9eff5543b1359ecb3bb1a7b49ba531
hildi70 <karla.berger@web.de>
2022-10-05 23:26:32 +0200

    fixed bug regarding well numeration

--- sorting_files.py
+++ sorting_files.py
@@ -24,10 +24,13 @@ Don't hesitate to contact me if there are questions, bugs, comments etc. Thanks 
 
 Contact:
 @author: Karla Berger
-@date: 2022-10-02
+@date: 2022-10-05
 @e-mail: karla.berger@web.de
 @github: https://github.com/hildi70/sorting_files
 """
+
+# TODO:
+# - add error messages
 
 import os
 import sys
@@ -40,18 +43,29 @@ import string
 os.chdir(os.path.dirname(sys.argv[0]))
 
 print("Hi there! Let's sort some data! :)")
-file_path = input("Which folder do you want to sort? E.g. \"Test\" ").rstrip("\n") + "/"
+file_path = input("Which folder do you want to sort? E.g. \"example_data\" ").rstrip("\n") + "/"
 start_well = input("What's the first well to analyze? E.g. \"A1\" ").rstrip("\n")
 stop_well = input("What's the last well to analyze? E.g. \"N22\" ").rstrip("\n")
 number_fields = input("How many fields are included? E.g. \"2\" ").rstrip("\n")
 
-start_letter = re.findall('\D', start_well)
-stop_letter = re.findall('\D', stop_well)
-letters = string.ascii_uppercase[string.ascii_uppercase.index(start_letter[0]):string.ascii_uppercase.index(stop_letter[0]) + 1]
+start_letter = re.search("\D", start_well).group().upper()
+stop_letter = re.search("\D", stop_well).group().upper()
+letters = string.ascii_uppercase[string.ascii_uppercase.index(start_letter):string.ascii_uppercase.index(stop_letter) + 1]
 
-start_number = re.findall('\d+', start_well)
-stop_number = re.findall('\d+', stop_well)
-numbers = ["%.2d" % i for i in range(int(start_number[0]), int(stop_number[0]) + 1 )]
+start_number = re.search("\d+", start_well).group()
+stop_number = re.search("\d+", stop_well).group()
+
+# extract well number of randomly selected file
+example_number = 0
+for file in glob.iglob(file_path + "*.tif"):
+    example_file = os.path.basename(file)
+    example_number = re.search("\d{1,3}\(", example_file).group()[:-1]
+    break
+
+if (len(example_number) > 1):
+    numbers = ["%.2d" % i for i in range(int(start_number), int(stop_number) + 1 )]
+else:
+    numbers = [i for i in range(int(start_number), int(stop_number) + 1 )]
 
 fields = []
 for i in range(1, int(number_fields) + 1):
@@ -61,10 +75,10 @@ for i in range(1, int(number_fields) + 1):
 for letter in letters:
     for number in numbers:
         for field in fields:
-            pattern = letter + "*" + number + "*" + field + "*.tif"
+            pattern = "*" + letter + "*" + str(number) + "*" + field + "*.tif"
             for file in glob.iglob(file_path + pattern):
-                file_name = os.path.basename(file) # ohne Leerzeichen
-                new_path = letter + "/" + number + "/" + field + "/"
+                file_name = os.path.basename(file)
+                new_path = letter + "/" + str(number) + "/" + field + "/"
                 new_file_name = file_name.replace(" ", "")
                 if not (os.path.exists(file_path + new_path)):
                     os.makedirs(file_path + new_path)
